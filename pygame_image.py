8c61ac494b627b7d29b0c259b7e4ba645bd04c9e
C0A24057 <c0a2405782@edu.teu.ac.jp>
2025-11-18 17:56:45 +0900

    演習２

--- pygame_image.py
+++ pygame_image.py
@@ -20,19 +20,21 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
+            
         x= tmr % 3200
+        vx = -1
+        vy = 0
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0,-1))
+            vy = -1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0,+1))
+            vy = +1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1,0))
+            vx = -2
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+1,0))
-        else:
-            kk_rct.move_ip(-1,0)
+            vx = +1
+
+        kk_rct.move_ip((vx,vy))
         screen.blit(bg_img, [-x,0])
         screen.blit(bg_img2,[-x+1600,0])
         screen.blit(bg_img,[-x+3200,0])
